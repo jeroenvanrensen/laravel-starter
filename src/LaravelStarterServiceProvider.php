dff62fdc5b58b0a0aa1da1c1c948e39c9cc940d6
Jeroen van Rensen <jmjrensen@jcjcj.nl>
2021-03-06 14:33:59 +0100

    Initialize the package

---
+++ src/LaravelStarterServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace JeroenvanRensen\LaravelStarter;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelStarterServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+        //
+    }
+
+    public function boot()
+    {
+        //
+    }
+}
